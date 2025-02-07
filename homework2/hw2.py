4557075cb53d8a0abd6bc5f713f0378ee84ac693
LMcGinn11 <lmcginn@berkeley.edu>
2025-02-06 21:34:23 -0800

    Committing homework2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,32 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# You could call: cd ../..
+
 # 2) What does ~/ mean?
+# ~/ refers to the home directory. The ~ is always included in your terminal's line to indicate that you're within your home directory.
+
 # 3) What's the difference between and abolute and a relative path in your terminal?
+# The absoute path takes you from the home directory to a certain file, while a relative path takes you from one directory to another.
+
 # 4) What command would bring you back to your home directory?
+# You could call: cd ~
+
 # 5) If you called rm ./ in your current working directory, what would it do? 
+# Just tried it, forgot rm meant remove and i panicked for a moment. But it said that it could not remove './' because it is a directory so i assume the command removes whatever file is associated with that name
+
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# git add enters your selected file(s) into the staging area
+# git commit saves changes to your local repository. You must always add a message
+# git push sends your code to your GitHub repository
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# git status will respond with "On branch master, No commits yet" and this is telling you whether you're in the master or main and tells you that nothing has been committed (i assume if something has been committed it would tell you)
+
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# Honestly, the only bug I've seen was with the first GitHub lecture when git push gave me a big scary error message and I had to call someone over
+
 # 9) How does cloning a repository differ from pulling from a repository?
+# Cloning a repository allows one to edit the contents of the repository clone while pulling from a repository allows one to edit only their copy of the files and doesn't allow one to make saves on the other repository
+
 # 10) Tell me a fun fact!
+# Isaac Newton tried many things to understand what color is. His many attempts to learn more include: Poking hard on his eyeball, staring at the sun for an extended period of time, pressing his eye against a copper plate, and finally, sticking a needle between his eye and his orbital bone. Gross! but he did it (kinda) so who are we to judge
+# Also titanium was first discovered in the southernmost tip of the UK
